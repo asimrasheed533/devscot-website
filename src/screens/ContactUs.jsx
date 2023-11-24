113e2cb5c6dca055b8001feca52ca27373fb9fc7
Asim Rasheed <asimrasheed442@gmail.com>
2023-11-24 13:15:15 +0500

    contact lest card done

--- src/screens/ContactUs.jsx
+++ src/screens/ContactUs.jsx
@@ -21,6 +21,65 @@ export default function ContactUs() {
           </div>
         </div>
       </div>
+      <div className="contact__card__row__wraper">
+        <div className="contact__card__row__wraper__col__left">
+          <div className="contact__card__row__wraper__col__form">
+            <div className="contact__card__col__form__text">
+              <div className="contact__card__col__form__heading">
+                <span>Drop</span> Us a line
+              </div>
+              <div className="contact__card__col__form__sub__heading">
+                Your email address will not be published. Required fields are
+                marked*
+              </div>
+            </div>
+            <div className="contact__card__row__wraper__col__form__input">
+              <input type="text" placeholder="Your Name" />
+            </div>
+            <div className="contact__card__row__wraper__col__form__input">
+              <input type="text" placeholder="Website" />
+            </div>
+            <div className="contact__card__row__wraper__col__form__input">
+              <input type="text" placeholder="Your Comment" />
+            </div>
+            <div className="contact__card__row__wraper__col__form__btn">
+              <div className="contact__card__row__wraper__col__form__btn__svg">
+                <svg
+                  width="64"
+                  height="64"
+                  viewBox="0 0 64 64"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle
+                    cx="32"
+                    cy="32"
+                    r="32"
+                    fill="url(#paint0_linear_687_2258)"
+                  />
+                  <defs>
+                    <linearGradient
+                      id="paint0_linear_687_2258"
+                      x1="7.03562"
+                      y1="3.04182"
+                      x2="68.4599"
+                      y2="12.0776"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stop-color="#6554DC" />
+                      <stop offset="1" stop-color="#4733B5" />
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </div>
+              <div className="contact__card__row__wraper__col__form__btn__text">
+                Get in Touch
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="contact__card__row__wraper__col"></div>
+      </div>
     </>
   );
 }
