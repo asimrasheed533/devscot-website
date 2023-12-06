3a3e363d48fc12a52e854cb4d750a63b9d6455e8
Asim Rasheed <asimrasheed442@gmail.com>
2023-12-06 09:57:25 +0500

    fixs

--- src/components/gridContainer.jsx
+++ src/components/gridContainer.jsx
@@ -1,38 +1,43 @@
 import React from "react";
 export function GridContainer({}) {
   return (
-    <div class="parent">
-      <div class="div1">
-        <img src="https://picsum.photos/1000/1000" />
-      </div>
-      <div class="normal">
-        <img src="https://picsum.photos/1000/1000" />
-      </div>
-      <div class="normal">
-        <img src="https://picsum.photos/1000/1000" />
-      </div>
-      <div class="normal">
-        <img src="https://picsum.photos/1000/1000" />
-      </div>
-      <div class="normal">
-        <img src="https://picsum.photos/1000/1000" />
-      </div>
-      <div class="normal">
-        <img src="https://picsum.photos/1000/1000" />
-      </div>
-      <div class="normal">
-        <img src="https://picsum.photos/1000/1000" />
-      </div>
-      <div class="normal">
-        <img src="https://picsum.photos/1000/1000" />
-      </div>
-      <div class="normal">
-        <img src="https://picsum.photos/1000/1000" />
-      </div>
-      <div class="normal">
-        <img src="https://picsum.photos/1000/1000" />
-      </div>
-      {/* <div class="normal2">
+    <>
+      <div className="main__grid__overlay">
+        <div className="services__container__main__wraper">
+          <div className="services__container__heading">
+            OUR <div className="sevrive__svg">Products</div>
+          </div>
+        </div>
+        <div class="parent">
+          <div class="div1">
+            <img src="https://picsum.photos/1000/1000" />
+          </div>
+          <div class="normal">
+            <img src="https://picsum.photos/1000/1000" />
+          </div>
+          <div class="normal">
+            <img src="https://picsum.photos/1000/1000" />
+          </div>
+          <div class="normal">
+            <img src="https://picsum.photos/1000/1000" />
+          </div>
+          <div class="normal">
+            <img src="https://picsum.photos/1000/1000" />
+          </div>
+          <div class="normal">
+            <img src="https://picsum.photos/1000/1000" />
+          </div>
+          <div class="normal">
+            <img src="https://picsum.photos/1000/1000" />
+          </div>
+          <div class="normal">
+            <img src="https://picsum.photos/1000/1000" />
+          </div>
+          <div class="normal">
+            <img src="https://picsum.photos/1000/1000" />
+          </div>
+
+          {/* <div class="normal2">
         <img src="https://picsum.photos/1000/1000" />
       </div>
       <div class="normal2">
@@ -41,6 +46,8 @@ export function GridContainer({}) {
       <div class="normal">
         <img src="https://picsum.photos/1000/1000" />
       </div> */}
-    </div>
+        </div>
+      </div>
+    </>
   );
 }
